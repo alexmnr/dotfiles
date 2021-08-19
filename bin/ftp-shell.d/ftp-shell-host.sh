e311a6395074f44f48561973bc9071e5936d08e6
Senaxius <alexander.minor004@gmail.com>
2021-08-19 21:27:05 +0200

    ftp shell update

--- bin/ftp-shell.d/ftp-shell-host.sh
+++ bin/ftp-shell.d/ftp-shell-host.sh
@@ -1,5 +1,18 @@
 while true; do
     sshpass -p "4tnaTvBN-2021" sftp -P 35878 reshell@surfactant.ddns.net:/home/reshell/ftp-shell/connections /home/senaex/bin/ftp-shell.d > /dev/null 2>&1 
-    echo $(cat /home/senaex/bin/ftp-shell.d/connections)
+    while read p; do
+        IP=$(echo "$p" | grep -o "I: .*" | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
+        PORT=$(echo "$p" | grep -o "P: .*" | grep -Eo "[0-9]{1,5}")
+        if [ -z "$IP" ]; then
+            echo "No connection found"
+        else
+            echo "Connecting to:"
+            echo $IP
+            echo $PORT
+            sshpass -p "Idkpuh-2021" ssh -R 42069:localhost:22 reshell@$IP -p $PORT sleep 10
+        fi
+        sleep 1
+    done </home/senaex/bin/ftp-shell.d/connections
+
 	sleep 3
 done
