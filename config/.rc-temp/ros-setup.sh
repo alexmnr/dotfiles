4867b196dac618bdc4778d58a049fbd60bd01ffb
Alexander Minor <alexander.minor004@gmail.com>
2022-12-01 13:31:00 +0100

    add rosco to path

--- config/.rc-temp/ros-setup.sh
+++ config/.rc-temp/ros-setup.sh
@@ -1,4 +1,14 @@
 source /opt/ros/humble/setup.zsh &> /dev/null
 source /home/ALEX/3dev/ros/install/setup.zsh &> /dev/null
+
 eval "$(register-python-argcomplete3 ros2)"
 eval "$(register-python-argcomplete3 colcon)"
+
+if [ -d /home/ALEX/3dev/docker ]; then
+    path+=(/home/ALEX/3dev/docker)
+fi
+
+if [ -d /root/3dev/docker ]; then
+    path+=(/root/3dev/docker)
+fi
+
