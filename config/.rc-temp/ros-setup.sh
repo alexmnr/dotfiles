60ceddca2d954217d1713179305340ac4a18219e
Senaxius <alexander.minor004@gmail.com>
2023-04-22 20:38:09 +0200

    anytrack update

--- config/.rc-temp/ros-setup.sh
+++ config/.rc-temp/ros-setup.sh
@@ -1,5 +1,5 @@
 source /opt/ros/humble/setup.zsh &> /dev/null
-source /home/ALEX/3dev/ros/install/setup.zsh &> /dev/null
+source /home/ALEX/anytrack/ros/install/setup.zsh &> /dev/null
 
 eval "$(register-python-argcomplete3 ros2)"
 eval "$(register-python-argcomplete3 colcon)"
