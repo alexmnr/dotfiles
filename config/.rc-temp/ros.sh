827703ea6bcf7a54e5281576e03495a7eafb3eb0
Senaxius <alexander.minor004@gmail.com>
2024-04-02 16:49:07 -0700

    new ros setup

--- config/.rc-temp/ros.sh
+++ config/.rc-temp/ros.sh
@@ -1,5 +1,18 @@
-source /opt/ros/humble/setup.zsh &> /dev/null
-source /home/ALEX/anytrack/ros/install/setup.zsh &> /dev/null
+if [ -d /opt/ros/humble/ ]; then
+  source /opt/ros/humble/setup.zsh &> /dev/null
+fi
+if [ -d /opt/ros/iron/ ]; then
+  source /opt/ros/iron/setup.zsh &> /dev/null
+fi
+if [ -d $HOME/Robot-Arm/ros ]; then
+  source $HOME/Robot-Arm/ros/install/setup.zsh
+fi
+if [ -d $HOME/arctos_ws ]; then
+  source $HOME/arctos_ws/install/setup.zsh
+fi
+if [ -d $HOME/moveit_ws ]; then
+  source $HOME/moveit/install/setup.zsh
+fi
 
 eval "$(register-python-argcomplete3 ros2)"
 eval "$(register-python-argcomplete3 colcon)"
