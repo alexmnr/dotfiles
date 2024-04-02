bbba3253d9376dfc814366e5c57391a0668f2e5c
Senaxius <alexander.minor004@gmail.com>
2024-04-02 16:56:24 -0700

    fixed ros.sh

--- config/.rc-temp/ros.sh
+++ config/.rc-temp/ros.sh
@@ -4,13 +4,14 @@ fi
 if [ -d /opt/ros/iron/ ]; then
   source /opt/ros/iron/setup.zsh &> /dev/null
 fi
-if [ -d $HOME/Robot-Arm/ros ]; then
+if [ -d $HOME/Robot-Arm/ros/install/ ]; then
   source $HOME/Robot-Arm/ros/install/setup.zsh
 fi
-if [ -d $HOME/arctos_ws ]; then
+if [ -d $HOME/arctos_ws/install/ ]; then
   source $HOME/arctos_ws/install/setup.zsh
 fi
-if [ -d $HOME/moveit_ws ]; then
+if [ -d $HOME/moveit_ws/install/ ]; then
+  echo "test"
   source $HOME/moveit/install/setup.zsh
 fi
 
