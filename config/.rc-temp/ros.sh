a1dc749df32d70825afec64e274ad74219181336
Senaxius <alexander.minor004@gmail.com>
2024-04-02 17:39:33 -0700

    updated ros setup

--- config/.rc-temp/ros.sh
+++ config/.rc-temp/ros.sh
@@ -4,15 +4,14 @@ fi
 if [ -d /opt/ros/iron/ ]; then
   source /opt/ros/iron/setup.zsh &> /dev/null
 fi
-if [ -d $HOME/Robot-Arm/ros/install/ ]; then
-  source $HOME/Robot-Arm/ros/install/setup.zsh
+if [ -d $HOME/Robot-Arm/ros/test_ws/install/ ]; then
+  source $HOME/Robot-Arm/ros/test_ws/install/setup.zsh
 fi
-if [ -d $HOME/arctos_ws/install/ ]; then
-  source $HOME/arctos_ws/install/setup.zsh
+if [ -d $HOME/Robot-Arm/ros/arctos_ws/install/ ]; then
+  source $HOME/Robot-Arm/ros/arctos_ws/install/setup.zsh
 fi
-if [ -d $HOME/moveit_ws/install/ ]; then
-  echo "test"
-  source $HOME/moveit/install/setup.zsh
+if [ -d $HOME/Robot-Arm/ros/moveit_ws/install/ ]; then
+  source $HOME/Robot-Arm/ros/moveit_ws/install/setup.zsh
 fi
 
 eval "$(register-python-argcomplete3 ros2)"
