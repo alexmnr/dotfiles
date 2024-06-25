8a18fb6ededf2e7fda621c5df215804616dc3cc7
Senaxius <alexander.minor004@gmail.com>
2024-06-25 09:00:22 -0700

    battery and tasks

--- config/awesome/theme/topbar/topbar.lua
+++ config/awesome/theme/topbar/topbar.lua
@@ -9,29 +9,85 @@ local color = require("theme.color")
 local mytextclock = wibox.widget.textclock(
 	'<span color="' .. color.white .. '" font="Ubuntu Nerd Font Bold 13"> %a %b %d, %H:%M </span>', 10)
 
+local separator = wibox.widget.textbox("     ")
+local batteryarc_widget = require("theme.topbar.batteryarc")
+local fancy_taglist = require("theme.topbar.fancy_taglist")
+local taglist_buttons = require("theme.topbar.taglist")
+local tasklist_buttons = require("theme.topbar.tasklist")
+
+--Fancy taglist widget
+awful.screen.connect_for_each_screen(function(s)
+	mytaglist = fancy_taglist.new({
+		screen   = s,
+		taglist  = { buttons = taglist_buttons },
+		tasklist = { buttons = tasklist_buttons },
+		filter   = awful.widget.taglist.filter.all,
+		style    = {
+			shape = gears.shape.rounded_rect
+		},
+	})
+end)
+--Taglist widget
+local fancy_taglist = wibox.widget {
+	{
+		mytaglist,
+		widget = wibox.container.background,
+		shape  = gears.shape.rounded_rect,
+		bg     = color.background_lighter
+	},
+	left   = dpi(3),
+	right  = dpi(3),
+	top    = dpi(3),
+	bottom = dpi(3),
+	widget = wibox.container.margin
+
+}
+
 local mywibox =
 	awful.wibar({
 		position = "top",
-		-- margins = { top = dpi(7), left = dpi(8), right = dpi(8), bottom = 0 },
 		margins = { top = dpi(3), left = dpi(3), right = dpi(3), bottom = 0 },
 		screen = s,
 		height = dpi(35),
 		opacity = 0.9,
 		fg = color.text,
 		bg = color.base,
-		-- shape = function(cr, width, height)
-		--   -- gears.shape.rounded_rect(cr, width, height, 8)
-		--   gears.shape.rounded_rect(cr, width, height, 0)
-		-- end,
-
 	})
 
 mywibox:setup {
-  layout = wibox.layout.align.horizontal,
+  layout = wibox.layout.stack,
   expand = "none",
+  {
+    layout = wibox.layout.align.horizontal,
+    {
+      -- Left widgets
+      layout = wibox.layout.fixed.horizontal,
+      fancy_taglist,
+      separator,
+    },
+    nil,
+    {
+      -- Right widgets
+      layout = wibox.layout.fixed.horizontal,
+      batteryarc_widget({
+        show_current_level = true,
+        arc_thickness = 3,
+        size = 30,
+        font = "CaskaydiaCove Nerd Font 10",
+        margins = 55,
+        timeout = 5,
+        charging_color = "#42f67b"
+      }),
+      wibox.widget.textbox("  "),
+
+      -- separator,
+    },
+  },
   { -- Middle widgets
-    layout = wibox.layout.flex.horizontal,
-    max_widget_size = 1500,
-    mytextclock
+    mytextclock,
+    valign = "center",
+    halign = "center",
+    layout = wibox.container.place,
   },
 }
+return mywibox
