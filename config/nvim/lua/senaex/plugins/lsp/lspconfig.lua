f187b4b4ff857d1028a13b76985b593a51672c78
Senaxius <alexander.minor004@gmail.com>
2024-09-22 11:33:26 -0700

    removed some none working stuff from the config

--- config/nvim/lua/senaex/plugins/lsp/lspconfig.lua
+++ config/nvim/lua/senaex/plugins/lsp/lspconfig.lua
@@ -83,12 +83,6 @@ return {
 			on_attach = on_attach,
 		})
 
-		-- configure typescript server with plugin
-		lspconfig["tsserver"].setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-		})
-
 		-- configure css server
 		lspconfig["cssls"].setup({
 			capabilities = capabilities,
