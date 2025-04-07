a71bb4aee74fefde319b40b5fe5cbb9242cb84af
alexander@minor.pw <alexander.minor004@gmail.com>
2025-04-07 15:51:44 -0700

    add pyright and clangd

--- nvim/lua/senaex/plugins/lsp/mason.lua
+++ nvim/lua/senaex/plugins/lsp/mason.lua
@@ -26,6 +26,8 @@ return {
       -- list of servers for mason to install
       ensure_installed = {
         "lua_ls",  -- Lua language server
+        "clangd",
+        "pyright",
       },
       -- auto-install configured servers (with lspconfig)
       automatic_installation = true, -- not the same as ensure_installed
